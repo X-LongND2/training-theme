973dfa019c962aa2a85e12e19180f1a5a2e65585
X-LongND2 <longnd2@bsscommerce.com>
2024-05-03 18:19:29 +0700

    Config tailwind to preprocess class+ done all collection settings+base UI and some logic settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{liquid,js}"],
+  content: [
+    "./src/**/*.{liquid,js}",
+    "./tailwind/pre-process-template/**/*.liquid"
+  ],
   theme: {
     extend: {},
   },
