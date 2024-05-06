c245e511669723cb4c50126f7458dbd57e0131da
X-LongND2 <longnd2@bsscommerce.com>
2024-05-06 15:02:46 +0700

    Done responsive for collection grid and slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     extend: {
       spacing: {
         '13': '3.25rem'
+      },
+      width: {
+        'slider-1': '100%',
+        'slider-2': 'calc((100% - 0.5rem)/2)',
+        'slider-3': 'calc((100% - 1rem)/3)',
+        'slider-4': 'calc((100% - 1.5rem)/4)',
+        'slider-5': 'calc((100% - 2rem)/5)',
+        'slider-6': 'calc((100% - 2.5rem)/6)',
       }
     },
   },
